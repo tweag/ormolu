02c6a58f27a49096be7ffe4ac70a39bad62fa671
mrkkrp <markkarpov92@gmail.com>
2018-12-08 17:34:26 +0700

    Implement basic parser for fooling around

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,27 @@
 module Main (main) where
 
+import Control.Monad
+import Ormolu.Parser
+import System.Environment (getArgs)
+import qualified Outputable as GHC
+
 main :: IO ()
-main = return ()
+main = do
+  (path:_) <- getArgs
+  input <- readFile path
+  (ws, r) <- parseModule [] path input
+  unless (null ws) $
+    putStrLn "dynamic option warnings:"
+    -- TODO print ws
+  case r of
+    Left (srcSpan, err) -> do
+      putStrLn (showOutputable srcSpan)
+      putStrLn err
+    Right (anns, parsedModule) -> do
+      putStrLn "\nannotations:\n"
+      putStrLn (showOutputable anns)
+      putStrLn "\nparsed module:\n"
+      putStrLn (showOutputable parsedModule)
+
+showOutputable :: GHC.Outputable o => o -> String
+showOutputable = GHC.showSDocUnsafe . GHC.ppr
--- src/Ormolu/Parser.hs
+++ src/Ormolu/Parser.hs
@@ -1,3 +1,22 @@
 module Ormolu.Parser
-  (  )
+  ( parseModule )
 where
+
+import Language.Haskell.GHC.ExactPrint.Parsers hiding (parseModule)
+import Language.Haskell.GHC.ExactPrint.Types
+import Ormolu.Type
+import qualified CmdLineParser as GHC
+import qualified DynFlags as GHC
+import qualified GHC hiding (parseModule)
+
+-- | Parse a complete module from string.
+
+parseModule
+  :: [DynOption]        -- ^ Dynamic options that affect parsing
+  -> FilePath           -- ^ File name (only for source location annotations)
+  -> String             -- ^ Input for parser
+  -> IO ([GHC.Warn], Either (GHC.SrcSpan, String) (Anns, GHC.ParsedSource))
+parseModule dynOpts path input = ghcWrapper $ do
+  dynFlags0 <- initDynFlagsPure path input
+  (dynFlags1, _, ws) <- GHC.parseDynamicFilePragma dynFlags0 (dynOption <$> dynOpts)
+  return (ws, parseModuleFromStringInternal dynFlags1 path input)
