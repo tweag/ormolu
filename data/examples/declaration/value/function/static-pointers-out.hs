06e4f1d5ad93849f65c9a6564c6554ec6c117244
Utku Demir <me@utdemir.com>
2019-05-28 13:22:18 +1200

    Implement TypeApplications and StaticPointers support

---
+++ data/examples/declaration/value/function/static-pointers-out.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE StaticPointers #-}
+foo :: StaticPtr Int
+foo = static 5
+
+bar :: StaticPtr [Int]
+bar =
+  static
+    [ 1
+    , 2
+    , 3
+    ]
+
+baz :: StaticPtr Bool
+baz =
+  static
+    ( fun 1
+      2
+    )
---
+++ data/examples/declaration/value/function/static-pointers.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE StaticPointers #-}
+
+foo :: StaticPtr Int
+foo = static 5
+
+bar :: StaticPtr [Int]
+bar = static [ 1
+             , 2, 3
+             ]
+
+baz :: StaticPtr Bool
+baz = static (fun 1
+                2)
