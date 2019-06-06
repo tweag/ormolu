46d175c89d8e8ee510152df386e6978aa567596c
mrkkrp <mark.karpov@tweag.io>
2019-06-06 22:29:50 +0200

    Make case expressions hanging

--- data/examples/declaration/value/function/case-multi-line-out.hs
+++ data/examples/declaration/value/function/case-multi-line-out.hs
@@ -1,25 +1,21 @@
 foo :: Int -> Int
-foo x =
-  case x of
-    5 -> 10
-    _ -> 12
+foo x = case x of
+  5 -> 10
+  _ -> 12
 
 bar :: Int -> Int
-bar x =
-  case x of
-    5 ->
-      if x > 5
-      then 10
-      else 12
-    _ -> 12
+bar x = case x of
+  5 ->
+    if x > 5
+    then 10
+    else 12
+  _ -> 12
 
 baz :: Int -> Int
-baz x =
-  case x of
-    5 -> 10
-    _ -> 12
+baz x = case x of
+  5 -> 10
+  _ -> 12
 
 quux :: Int -> Int
-quux x =
-  case x of
-    x -> x
+quux x = case x of
+  x -> x
