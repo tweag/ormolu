086cddf2cf7dfee85adc7d6f0263bcf682ddd9fa
Basile Henry <bjm.henry@gmail.com>
2019-06-15 15:38:58 +0200

    Add CompleteMatchSig printer

---
+++ data/examples/declaration/signature/complete/complete-out.hs
@@ -0,0 +1,10 @@
+{-# COMPLETE A, B, C :: Foo #-}
+
+{-# COMPLETE A, B #-}
+
+{-# COMPLETE
+  A
+  , B
+  , C
+    :: Foo
+  #-}
---
+++ data/examples/declaration/signature/complete/complete.hs
@@ -0,0 +1,13 @@
+{-#   ComPlETe       A  , B, C::         Foo
+
+  #-}
+
+{-#    COMPLETE          A , B
+   #-}
+
+{-#   ComPlETE
+      A  , 
+    B, C
+
+
+    ::         Foo#-}
