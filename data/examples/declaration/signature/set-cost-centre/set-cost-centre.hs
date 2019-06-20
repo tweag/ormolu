741e907cd882d3695316401d110bb25967d2e2d0
sphaso <sphaso@gmail.com>
2019-06-20 20:23:55 +0200

    Implemented set cost centre pragma

---
+++ data/examples/declaration/signature/set-cost-centre/set-cost-centre.hs
@@ -0,0 +1,16 @@
+f x y = g (x + y)
+  where
+    g z = z
+    {-# SCC g #-}
+
+{-# SCC f #-}
+
+withString x y = x + y
+
+{-# SCC withString "cost_centre_name" #-}
+
+withString' x y = x + y
+
+{-# SCC withString'
+  "cost_centre_name"
+  #-}
