3596fff697348d58887e8df2922bb23ca13146a9
Dmitry Ivanov <ethercrow@gmail.com>
2019-06-27 21:03:57 +0200

    Add more function guards tests

--- data/examples/declaration/value/function/guards.hs
+++ data/examples/declaration/value/function/guards.hs
@@ -10,3 +10,16 @@ bar x
   | x == 6 = foo x
        + foo 20
   | otherwise = foo 100
+
+baz :: Int -> Int
+baz x | x < 0 = x
+baz x | otherwise = x
+
+multi_baz :: Int -> Int
+multi_baz x | x < 42 = x
+multi_baz x | x < 0 = x
+multi_baz x | otherwise = x
+
+quux :: Int -> Int
+quux x | x < 0 = x
+quux x = x
