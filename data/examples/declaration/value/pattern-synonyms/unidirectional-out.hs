5f5f4419691c59a70ff628106fd044cface6cd3c
Mauricio Fierro <mauriciofierrom@gmail.com>
2019-07-15 18:26:49 -0500

    Fix record pattern synonyms

--- data/examples/declaration/value/pattern-synonyms/unidirectional-out.hs
+++ data/examples/declaration/value/pattern-synonyms/unidirectional-out.hs
@@ -5,6 +5,18 @@ pattern Head x <- x : xs
 pattern Head' x <-
   x : xs
 
+pattern Head'' {x} <-
+  x : xs
+
+pattern FirstTwo {x, y} <-
+  x : (y : xs)
+
+pattern FirstTwo'
+  { x,
+    y
+    } <-
+  x : (y : xs)
+
 pattern Simple <- "Simple"
 
 pattern WithTypeSig :: String
