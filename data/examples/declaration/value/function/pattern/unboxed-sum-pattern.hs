be53b3e5049c6173daf0ced0f2243ee5b1b6297c
gabriele.lana <gabriele.lana@gmail.com>
2019-07-20 16:49:18 +0200

    Format unboxed sum pattern

---
+++ data/examples/declaration/value/function/pattern/unboxed-sum-pattern.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE UnboxedSums #-}
+
+v = True
+  where
+    (# _x #) = (# True #)
+
+p = True
+  where
+    (# _x | #) = (# | True #)
+
+q = True
+  where
+    (# | _x | #) = (# | True | #)
+
+z = True
+  where
+    (# | | _x #) = (# | | True #)
+
+z_multiline = True
+  where
+    (# |
+       | _x #) = (# |
+                    | True #)
