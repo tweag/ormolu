2b5283030b201310eb51f5b496571e1f3da7c9c8
mrkkrp <mark.karpov@tweag.io>
2019-08-05 22:17:28 +0200

    Avoid failing when comments get before an operator
    This puts out the fire, but I'm not fully content with the solution. I also
    do not understand why it fails in the original issue but succeeds for e.g.:
    
      foo = do
        1
        +
        2

---
+++ data/examples/declaration/value/other/comments-get-before-op.hs
@@ -0,0 +1,11 @@
+main :: IO ()
+main = do
+    migrateSchema
+        [ migration1
+        , migration1
+        , migration3
+        -- When adding migrations here, don't forget to update
+        -- 'schemaVersion' in Galley.Data
+        ]
+      `finally`
+        Log.close
