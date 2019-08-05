a8bcb91f64f57d980887d571b54d7aef37058b8c
mrkkrp <mark.karpov@tweag.io>
2019-08-05 21:15:01 +0200

    Fine-tune placement of lambdas after operators

--- data/examples/declaration/value/function/lambda-multi-line.hs
+++ data/examples/declaration/value/function/lambda-multi-line.hs
@@ -7,3 +7,16 @@ bar x = \y ->
   if x > y
     then 10
     else 12
+
+tricky0 =
+  flip all (zip ws gs) $ \(wt, gt) ->
+    canUnify poly_given_ok wt gt || go False wt gt
+
+tricky1 =
+  flip all (zip ws gs) $
+  \(wt, gt) -> canUnify poly_given_ok wt gt || go False wt gt
+
+tricky2 =
+  flip all (zip ws gs) $
+  \(wt, gt) ->
+    canUnify poly_given_ok wt gt || go False wt gt
