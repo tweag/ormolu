6e12ee8da7eedb6fd5ff5404abce9cff8bb3f708
Utku Demir <me@utdemir.com>
2019-08-09 09:49:58 +1200

    Workaround GHC tokenizer bug while extracting LANGUAGE pragmas

--- data/examples/other/language-pragma.hs
+++ data/examples/other/language-pragma.hs
@@ -1,7 +1,10 @@
 -- | Header comment.
 
 {-# LANGUAGE LambdaCase #-}
-{-# LANGuagE ViewPatterns #-}
+{-#LANGuagE ViewPatterns #-}
+{-# LANGUAGE OverloadedStrings#-}
 {-# language DataKinds, LambdaCase #-}
+{-# language IncoherentInstances
+           , AllowAmbiguousTypes #-}
 
 module Foo () where
