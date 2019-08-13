acc77002e44e9bf9fb04e909b2ea63599d93ee0f
Utku Demir <me@utdemir.com>
2019-08-14 11:49:07 +1200

    Support inline specs inside specialize pragmas

--- data/examples/declaration/signature/specialize/specialize-out.hs
+++ data/examples/declaration/signature/specialize/specialize-out.hs
@@ -1,9 +1,9 @@
 foo :: Num a => a -> a
 foo = id
 {-# SPECIALIZE foo :: Int -> Int #-}
-{-# SPECIALIZE foo :: Float -> Float #-}
+{-# SPECIALIZE INLINE foo :: Float -> Float #-}
 
-{-# SPECIALIZE [2] bar :: Int -> Int #-}
+{-# SPECIALIZE NOINLINE [2] bar :: Int -> Int #-}
 bar :: Num a => a -> a
 bar = id
 
