33108051d99fbf399c4d7b5bf7d3f81e8402c416
Utku Demir <me@utdemir.com>
2019-08-15 19:29:05 +1200

    Hang record constructor fields

--- data/examples/declaration/value/function/record-constructors.hs
+++ data/examples/declaration/value/function/record-constructors.hs
@@ -4,3 +4,19 @@ bar = Bar {
     def = Foo {a = 10}
 }
 baz = Baz{ }
+
+aLongVariableName =
+  ALongRecordName
+    { short = baz,
+      aLongRecordFieldName = YetAnotherLongRecordName
+        { yetAnotherLongRecordFieldName = "a long string"
+          },
+      aLongRecordFieldName2 = Just YetAnotherLongRecordName
+                                 { yetAnotherLongRecordFieldName = "a long string",
+                                   yetAnotherLongRecordFieldName = Just
+                                                                     "a long string"
+                                   },
+      aLongRecordFieldName3 = do
+                               foo
+                               bar
+      }
