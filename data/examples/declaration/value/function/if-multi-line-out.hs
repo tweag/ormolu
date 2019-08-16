f94400b4860df45fcd72420401393f98abc38445
mrkkrp <mark.karpov@tweag.io>
2019-08-16 22:24:11 +0200

    Indent ‘then’ and ‘else’ clauses one level further
    This arguably improves readability especially when if-then-else is placed
    inside of a ‘do’-block.

--- data/examples/declaration/value/function/if-multi-line-out.hs
+++ data/examples/declaration/value/function/if-multi-line-out.hs
@@ -1,16 +1,16 @@
 foo :: Int -> Int
 foo x =
   if x > 5
-  then 10
-  else 12
+    then 10
+    else 12
 
 bar :: Int -> Int
 bar x =
   if x > 5
-  then
-    foo x
-      + 100
-  else
-    case x of
-      1 -> 10
-      _ -> 20
+    then
+      foo x
+        + 100
+    else
+      case x of
+        1 -> 10
+        _ -> 20
