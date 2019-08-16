9d77dad93b34e64c22b7dede2cc8c9dc67704cf3
Utku Demir <me@utdemir.com>
2019-08-16 12:24:34 +1200

    Hang after block arguments

---
+++ data/examples/declaration/value/function/block-arguments.hs
@@ -0,0 +1,18 @@
+f1 = foo do bar
+
+f2 = foo do
+  bar
+
+f3 = foo case True of
+  True -> bar
+  False -> baz
+
+f4 = foo let a = 3 in b
+
+f5 = foo let a = 3
+             b = a
+          in b
+
+f6 = foo if bar
+         then baz
+         else not baz
