77424882fda8f97087bba57727c2c84c8225e21c
Avi Dessauer <avi.the.coder@gmail.com>
2020-01-14 04:59:10 -0500

    Differentiate recordDot & standard record updates

--- data/examples/declaration/value/function/record-dot-out.hs
+++ data/examples/declaration/value/function/record-dot-out.hs
@@ -5,3 +5,11 @@ data Foo = Foo {bar :: Int}
 mfoo = fmap (.bar) $ Nothing
 
 bar = (Foo 1).bar
+
+fooplus f n = f{foo = f.bar + n}
+
+fooplus' f n = f {foo = f.bar + n}
+
+fooplus'' f n = f {foo = f.bar + n}
+
+fooplus''' f n = f {foo = f.bar + n}
--- data/examples/declaration/value/function/record-dot.hs
+++ data/examples/declaration/value/function/record-dot.hs
@@ -4,3 +4,11 @@ data Foo = Foo { bar :: Int }
 mfoo = fmap (.bar)   $ Nothing
 
 bar = (  Foo 1).bar
+
+fooplus f n = f{foo = f.bar + n}
+
+fooplus' f n = f { foo = f.bar + n}
+
+fooplus'' f n = f {foo = f.bar + n}
+
+fooplus''' f n = f{ foo = f.bar + n}
