16d8e67c51f2531662cb445a9886001ca1baed94
Mark Karpov <markkarpov92@gmail.com>
2020-05-11 12:26:39 +0200

    Fix the false positive about AST differences
    Comments that have more than one consecutive blank line will be rendered
    with just a single blank line due to how our rendering engine works. I think
    that the engine work correctly in this case and we want to generally
    normalize blank lines. So what should be fixed is just the check. For that
    we remove extra consecutive blank lines when we construct ‘Comment’ values.

---
+++ data/examples/other/multiple-blank-line-comment-out.hs
@@ -0,0 +1,9 @@
+module A where
+
+a =
+  b
+    [ f
+    {-,
+
+                                       -}
+    ]
---
+++ data/examples/other/multiple-blank-line-comment.hs
@@ -0,0 +1,10 @@
+module A where
+
+a =
+  b
+    [ f
+ {-,
+
+
+                                    -}
+    ]
