54d36fcc2cfb61d074a4f9035969eb3875325554
Mark Karpov <markkarpov92@gmail.com>
2020-08-22 22:48:38 +0200

    Update nix pin, test with GHC 8.8.4

--- nix/nixpkgs/default.nix
+++ nix/nixpkgs/default.nix
@@ -1,7 +1,7 @@
 { system ? builtins.currentSystem }:
 let
-  rev = "807ca93fadd5197c2260490de0c76e500562dc05";
-  sha256 = "10yq8bnls77fh3pk5chkkb1sv5lbdgyk1rr2v9xn71rr1k2x563p";
+  rev = "32b46dd897ab2143a609988a04d87452f0bbef59";
+  sha256 = "1gzfrpjnr1bz9zljsyg3a4zrhk8r927sz761mrgcg56dwinkhpjk";
   nixpkgs = builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
     inherit sha256;
