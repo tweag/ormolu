288e1cc039ba754446ecf2456e531b97e14a410b
Greg Pfeil <greg.pfeil@kittyhawk.aero>
2020-11-10 12:51:55 -0700

    Add support for monad comprehensions

---
+++ data/examples/declaration/value/function/comprehension/transform-monad-out.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE MonadComprehensions #-}
+{-# LANGUAGE TransformListComp #-}
+
+quux' xs ys =
+  [ ( x,
+      y
+    )
+    | x <- xs,
+      y <- ys,
+      then group by
+        -- First comment
+        ( x
+            + y
+        )
+      using
+        -- Second comment
+        groupWith -- Third comment
+  ]
---
+++ data/examples/declaration/value/function/comprehension/transform-monad.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE MonadComprehensions #-}
+{-# LANGUAGE TransformListComp #-}
+
+quux' xs ys = [
+  (x,
+    y) |
+  x <- xs,
+  y <- ys,
+  then
+  group
+  by -- First comment
+    (x
+      + y)
+  using -- Second comment
+    groupWith -- Third comment
+  ]
