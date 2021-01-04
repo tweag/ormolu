88e2e62a1e1d1745766ad0ef44fd8b9f7f4f4f6c
Mark Karpov <markkarpov92@gmail.com>
2021-01-04 21:36:50 +0100

    Test with GHC 8.10.3

--- nix/nixpkgs/default.nix
+++ nix/nixpkgs/default.nix
@@ -1,7 +1,7 @@
 { system ? builtins.currentSystem }:
 let
-  rev = "84d74ae9c9cbed73274b8e4e00be14688ffc93fe";
-  sha256 = "0ww70kl08rpcsxb9xdx8m48vz41dpss4hh3vvsmswll35l158x0v";
+  rev = "56bb1b0f7a33e5d487dc2bf2e846794f4dcb4d01";
+  sha256 = "1wl5yglgj3ajbf2j4dzgsxmgz7iqydfs514w73fs9a6x253wzjbs";
   nixpkgs = builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
     inherit sha256;
