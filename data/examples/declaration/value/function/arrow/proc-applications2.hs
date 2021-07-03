c089bb1223dc3131140f338846125e18820faf4e
Robert Vollmert <rob@vllmrt.net>
2021-07-03 14:13:24 +0200

    Implement HsCmdApp (fixes #716)

---
+++ data/examples/declaration/value/function/arrow/proc-applications2.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE Arrows #-}
+
+t = proc ys ->
+  (| f (\y -> returnA -< y) |) ys
+
+g x = proc (y, z) ->
+  ( case compare x y of
+      LT -> \a -> returnA -< x + a
+      EQ -> \b -> returnA -< y + z + b
+      GT -> \c -> returnA -< z + x
+  ) 1
