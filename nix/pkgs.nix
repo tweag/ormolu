906b20dfe1108fe66fe52d2987d3b387c2d34274
amesgen <amesgen@amesgen.de>
2021-11-17 14:46:26 +0100

    Rebuild all GHCJS dependencies by upgrading haskell.nix

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -4,13 +4,6 @@ let
   inherit (haskellNix) nixpkgsArgs;
   overlays = nixpkgsArgs.overlays ++ [
     (self: super: {
-      closurecompiler = super.closurecompiler.overrideAttrs (old: rec {
-        version = "20211107";
-        src = super.fetchurl {
-          url = "https://repo1.maven.org/maven2/com/google/javascript/closure-compiler/v${version}/closure-compiler-v${version}.jar";
-          sha256 = "733f00f0a1651c9d5409d9162e6f94f0a3e61463628925d3d6ef66be60ec14a6";
-        };
-      });
       macdylibbundler = super.macdylibbundler.overrideAttrs (old: {
         version = "custom";
         src = super.fetchFromGitHub {
