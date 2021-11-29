39aadbf4cc68df2630067fb3fe3194680a82949e
Thomas BAGREL <tomsb07@gmail.com>
2021-11-29 12:08:47 +0100

    Change example names
    For example, `op-chain-r-lt-normal` means
      Operator chain with right associativity, with two kinds of operators, where p(a) < p(b)

--- data/examples/declaration/value/function/infix/op-chain-r-lt-colon-2.hs
+++ data/examples/declaration/value/function/infix/op-chain-r-lt-colon-2.hs
@@ -1,11 +1,11 @@
 -- Right chain, : case, 2 operators with p(a) < p(:)
 m :: Int
 m =
-  1 `seq`
-  2 `seq`
-  3 `seq`
+  1 ||
+  2 ||
+  3 ||
   4 :
-  5 `seq`
-  6 `seq`
-  7 `seq`
+  5 ||
+  6 ||
+  7 ||
   8
