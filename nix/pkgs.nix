376ffcccb5aa621c408e357c86d77248ae82bb96
Alexander Esgen <alexander.esgen@tweag.io>
2022-02-15 14:51:38 +0100

    Bump nixpkgs via haskell.nix for macdylibbundler 1.0.4

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -1,19 +1,5 @@
 let
   sources = import ./sources.nix { };
   haskellNix = import sources.haskellNix { };
-  inherit (haskellNix) nixpkgsArgs;
-  overlays = nixpkgsArgs.overlays ++ [
-    (self: super: {
-      macdylibbundler = super.macdylibbundler.overrideAttrs (old: {
-        version = "custom";
-        src = super.fetchFromGitHub {
-          owner = "amesgen";
-          repo = "macdylibbundler";
-          rev = "f98d06980e718947b1d4afed3586f475056563b6";
-          sha256 = "0qqgcp14vvr6r5yqj96hy3klc2dkcqyq909az30ql4y879r3xm62";
-        };
-      });
-    })
-  ];
 in
-import haskellNix.sources.nixpkgs-unstable (nixpkgsArgs // { inherit overlays; })
+import haskellNix.sources.nixpkgs-unstable haskellNix.nixpkgsArgs
