b23b8b9914327dec512526d5b7c053047c18683e
Mark Karpov <mark.karpov@tweag.io>
2022-05-09 16:18:55 +0200

    Fix formatting of single-constructor record with a Haddock

---
+++ data/examples/declaration/data/single-constructor-with-haddock.hs
@@ -0,0 +1,6 @@
+data FutureSumType = -- | This is what the documentation for this type looks like.
+  -- It is pretty awkward that the @--@s aren't aligned.
+  SingleConstructor
+  { someRecordField :: Bool,
+    someOtherRecordField :: Int
+  }
