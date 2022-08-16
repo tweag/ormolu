2edda65fa4abb29fa164a6f6a1507d9188c221a2
Matthew Healy <matthew.healy@tweag.io>
2022-08-16 14:23:34 +0200

    Load clipboard.js at app startup

--- ormolu-live/src/Language/Javascript/JSaddle/Warp/Extra.hs
+++ ormolu-live/src/Language/Javascript/JSaddle/Warp/Extra.hs
@@ -1,12 +1,14 @@
 module Language.Javascript.JSaddle.Warp.Extra (run) where
 
 #ifndef ghcjs_HOST_OS
+import qualified Data.ByteString.Lazy as B
 import Language.Javascript.JSaddle.Run (syncPoint)
 import Language.Javascript.JSaddle.Types (JSM)
 import Language.Javascript.JSaddle.WebSockets
 import qualified Network.Wai.Application.Static as WaiStatic
 import qualified Network.Wai.Handler.Warp as Warp
 import qualified Network.WebSockets as WS
+import System.FilePath ( (</>) )
 #endif
 
 #ifdef ghcjs_HOST_OS
@@ -15,8 +17,9 @@ run _ _ = id
 #else
 run :: Int -> FilePath -> JSM () -> IO ()
 run port dir f = do
+  clipboardJs <- B.readFile (dir </> "clipboard.min.js")
   let staticApp = WaiStatic.staticApp $ WaiStatic.defaultFileServerSettings dir
-      app = jsaddleAppWithJsOr (jsaddleJs False) staticApp
+      app = jsaddleAppWithJsOr (jsaddleJs False <> clipboardJs) staticApp
   app <- jsaddleOr WS.defaultConnectionOptions (f *> syncPoint) app
   Warp.runSettings (Warp.setPort port . Warp.setTimeout 3600 $ Warp.defaultSettings) app
 #endif
