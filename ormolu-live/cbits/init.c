183a9f4deede12530938f92d9864973a291f491a
Cheng Shao <terrorjack@type.dance>
2023-10-23 19:00:42 +0000

    Improve ormolu live wasm size

--- ormolu-live/cbits/init.c
+++ ormolu-live/cbits/init.c
@@ -1,9 +1,21 @@
+#include "Rts.h"
+
 #include "Main_stub.h"
-#include <Rts.h>
+
+void malloc_inspect_all(void (*handler)(void *start, void *end,
+                                        size_t used_bytes, void *callback_arg),
+                        void *arg);
+
+static void malloc_inspect_all_handler(void *start, void *end,
+                                       size_t used_bytes, void *callback_arg) {
+  if (used_bytes == 0) {
+    memset(start, 0, (size_t)end - (size_t)start);
+  }
+}
 
 __attribute__((export_name("wizer.initialize"))) void __wizer_initialize(void) {
   char *args[] = {
-      "ormolu-live.wasm", "+RTS", "--nonmoving-gc", "-H64m", "-RTS", NULL};
+      "ormolu-live.wasm", "+RTS", "-H64m", "-RTS", NULL};
   int argc = sizeof(args) / sizeof(args[0]) - 1;
   char **argv = args;
   hs_init_with_rtsopts(&argc, &argv);
@@ -11,4 +23,6 @@ __attribute__((export_name("wizer.initialize"))) void __wizer_initialize(void) {
   hs_perform_gc();
   hs_perform_gc();
   rts_clearMemory();
+  malloc_inspect_all(malloc_inspect_all_handler, NULL);
+  memset(0, 0, (size_t)__builtin_frame_address(0));
 }
