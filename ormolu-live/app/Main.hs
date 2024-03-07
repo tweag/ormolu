41db79b8c08f5a51fd7b3dfcdd1bd553019094d1
Alexander Esgen <alexander.esgen@tweag.io>
2024-03-07 16:28:12 +0100

    [WIP] Switch Ormolu Live WASM to Miso

--- ormolu-live/app/Main.hs
+++ ormolu-live/app/Main.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedLabels #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ViewPatterns #-}
@@ -10,51 +12,192 @@ module Main where
 
 import Control.DeepSeq (force)
 import Control.Exception qualified as E
-import Data.Aeson qualified as A
-import Data.ByteString.Lazy qualified as BL
-import Data.ByteString.Unsafe qualified as BU
+import Control.Lens
+import Data.Bool (bool)
 import Data.Functor (void)
+import Data.Generics.Labels ()
+import Data.List (intersperse)
 import Data.Map.Strict qualified as Map
 import Data.Set (Set)
 import Data.Set qualified as Set
 import Data.Text (Text)
 import Data.Text qualified as T
 import Distribution.Types.PackageName (PackageName)
-import Foreign hiding (void)
-import Foreign.C.Types
 import GHC.Driver.Ppr (showSDocUnsafe)
 import GHC.Generics (Generic)
 import GHC.Hs.Dump qualified as Dump
-import Ormolu
+import Language.Javascript.JSaddle.Wasm qualified as JSaddle.Wasm
+import Miso
+import Miso.String (MisoString, fromMisoString, ms)
+import Ormolu qualified as O
 import Ormolu.Config qualified as O
 import Ormolu.Exception qualified as O
 import Ormolu.Fixity qualified as O
 import Ormolu.Parser qualified as O
 import Ormolu.Parser.Result as O
 import Ormolu.Terminal qualified as O
+import System.IO.Unsafe (unsafePerformIO)
+import UnliftIO.Exception
+
+type OrmoluConfig = O.Config O.RegionIndices
+
+data Output
+  = FormattingFailed SomeException
+  | FormattingSucceeded Text
+  deriving stock (Show, Generic)
+
+instance Eq Output where
+  FormattingSucceeded t == FormattingSucceeded t' = t == t'
+  FormattingFailed _ == FormattingFailed _ = True
+  _ == _ = False
+
+data Model = Model
+  { input :: MisoString,
+    output :: Output,
+    config :: OrmoluConfig,
+    showParseResult :: Bool
+  }
+  deriving stock (Show, Eq, Generic)
+
+data Action
+  = NoOp
+  | SetInput MisoString
+  | SetOutput Output
+  | Format
+  | UpdateConfig (OrmoluConfig -> OrmoluConfig)
+  | SetShowParseResult Bool
+
+foreign export javascript "hs_start" main :: IO ()
 
 main :: IO ()
-main = mempty
+main = JSaddle.Wasm.run $ startApp App {..}
+  where
+    initialAction = NoOp
+    model = Model {..}
+      where
+        input = ""
+        output = FormattingSucceeded ""
+        config = O.defaultConfig {O.cfgCheckIdempotence = True}
+        showParseResult = False
+    update = fromTransition . updateModel
+    view = viewModel
+    events = defaultEvents
+    subs = []
+    mountPoint = Nothing
+    logLevel = Off
 
--- marshalling
+updateModel :: Action -> Transition Action Model ()
+updateModel = \case
+  NoOp -> pure ()
+  SetInput t -> do
+    #input .= t
+    format
+  SetOutput o ->
+    #output .= o
+  Format -> do
+    input <- fromMisoString <$> use #input
+    runOrmolu input $ scheduleIO . fmap SetOutput
+  UpdateConfig f -> do
+    #config %= f
+    format
+  SetShowParseResult b ->
+    #showParseResult .= b
+  where
+    format = scheduleIO $ pure Format
+    runOrmolu input schedule = do
+      config <- use #config
+      schedule $
+        tryAnyDeep (O.ormolu config "<input>" input) <&> \case
+          Right t -> FormattingSucceeded t
+          Left e -> FormattingFailed e
 
-foreign export ccall mallocPtr :: IO (Ptr (Ptr a))
+viewModel :: Model -> View Action
+viewModel model@Model {..} =
+  div_
+    []
+    [ link_ [rel_ "stylesheet", href_ "bulma.min.css"],
+      section_ [class_ "section"] . pure . div_ [class_ "container is-fluid"] $
+        [ h1_ [class_ "title"] [text "Ormolu Live"],
+          div_
+            [class_ "content"]
+            [ p_
+                []
+                [ text $ "Version " <> VERSION_ormolu <> ", commit ",
+                  a_
+                    [href_ $ "https://github.com/tweag/ormolu/commit/" <> "XXX", target_ "blank"]
+                    [span_ [class_ "is-family-code"] [text . ms . T.take 7 $ "XXX"]],
+                  text $ ", using ghc-lib-parser " <> VERSION_ghc_lib_parser
+                ],
+              p_
+                []
+                [ a_
+                    [class_ "button is-link is-light", href_ "https://github.com/tweag/ormolu", target_ "blank"]
+                    [text "See the GitHub repository"]
+                ],
+              div_ [] . intersperse (br_ []) $
+                [ configCheckbox
+                    #cfgCheckIdempotence
+                    "Check idempotence (formatting twice is the same as formatting once)",
+                  configCheckbox
+                    #cfgUnsafe
+                    "Unsafe mode (don't ensure that formatting preserves the AST)",
+                  configCheckbox
+                    (#cfgSourceType . iso (== O.SignatureSource) (bool O.ModuleSource O.SignatureSource))
+                    "Format a Backpack signature",
+                  checkbox (^. #showParseResult) SetShowParseResult "Show internal parse result"
+                ]
+            ],
+          div_
+            [class_ "columns"]
+            [ div_
+                [class_ "column is-half is-flex"]
+                [ textarea_
+                    [class_ "textarea is-family-code", onInput SetInput, rows_ "20", autofocus_ True]
+                    [text input]
+                ],
+              div_ [class_ "column is-half is-flex"] [out]
+            ]
+        ]
+          <> [ div_
+                 [class_ "columns"]
+                 [ pre_
+                     [class_ "column is-half is-family-code"]
+                     [ text . ms . prettyAST config . fromMisoString $ input
+                     ],
+                   pre_
+                     [class_ "column is-half is-family-code"]
+                     [text . ms . prettyAST config $ m | FormattingSucceeded m <- [output]]
+                 ]
+               | showParseResult
+             ]
+    ]
+  where
+    checkbox fromModel action desc =
+      label_
+        [class_ "checkbox"]
+        [ input_
+            [ type_ "checkbox",
+              checked_ $ fromModel model,
+              onChecked \(Checked c) -> action c
+            ],
+          text $ " " <> desc
+        ]
+    configCheckbox (cloneLens -> l) = checkbox (^. #config . l) \c -> UpdateConfig $ l .~ c
 
-mallocPtr :: IO (Ptr (Ptr a))
-mallocPtr = malloc
+    out = case output of
+      FormattingSucceeded t ->
+        pre_ [class_ "is-family-code is-flex-grow-1"] [text t]
+      FormattingFailed e
+        | Just oe <- fromException e ->
+            pre_
+              [class_ "is-flex-grow-1 content has-background-danger-light has-text-danger-dark"]
+              [text . ms . O.runTermPure . O.printOrmoluException $ oe]
+        | otherwise ->
+            pre_
+              [class_ "is-flex-grow-1 content has-background-danger-light has-text-danger-dark"]
+              [text . ms . show $ e]
 
-foreign export ccall formatRaw :: Ptr CChar -> Int -> Ptr (Ptr CChar) -> IO Int
-
-formatRaw :: Ptr CChar -> Int -> Ptr (Ptr CChar) -> IO Int
-formatRaw inputPtr inputLen outputPtrPtr = do
-  Just input <-
-    A.decodeStrict' <$> BU.unsafePackMallocCStringLen (inputPtr, inputLen)
-  outputBytes <- BL.toStrict . A.encode <$> format input
-  BU.unsafeUseAsCStringLen outputBytes \(buf, len) -> do
-    outputPtr <- mallocBytes len
-    poke outputPtrPtr outputPtr
-    copyBytes outputPtr buf len
-    pure len
+-- Wizer
 
 foreign export ccall evaluateFixityInfo :: IO ()
 
@@ -63,42 +206,6 @@ evaluateFixityInfo =
   void . E.evaluate $ force (O.hackageInfo, liveDepencencies)
 
 -- actual logic
-
-data Input = Input
-  { inputStr :: Text,
-    checkIdempotence :: Bool,
-    unsafeMode :: Bool,
-    formatBackpack :: Bool,
-    showAST :: Bool
-  }
-  deriving stock (Show, Generic)
-  deriving anyclass (A.FromJSON)
-
-data Output = Output
-  { fmtStr :: Text,
-    inputAST :: Text,
-    outputAST :: Text
-  }
-  deriving stock (Show, Generic)
-  deriving anyclass (A.ToJSON)
-
-format :: Input -> IO Output
-format Input {..} = do
-  output <-
-    (Right <$> ormolu cfg "<input>" inputStr) `E.catch` (pure . Left . O.runTermPure . O.printOrmoluException)
-  inputAST <- if showAST then prettyAST cfg inputStr else pure T.empty
-  outputAST <- case output of
-    Right src' | showAST -> prettyAST cfg src'
-    _ -> pure T.empty
-  pure Output {fmtStr = either id id output, ..}
-  where
-    cfg =
-      defaultConfig
-        { cfgCheckIdempotence = checkIdempotence,
-          cfgUnsafe = unsafeMode,
-          cfgSourceType = if formatBackpack then SignatureSource else ModuleSource,
-          cfgDependencies = liveDepencencies
-        }
 
 -- | We want to make as many packages as possible available by default, so we
 -- only exclude packages that contain modules with the same name as in certain
@@ -116,8 +223,8 @@ liveDepencencies =
       pkgName `Set.member` priorityPkgs
         || modules `Set.disjoint` priorityModules
 
-prettyAST :: Config RegionIndices -> Text -> IO Text
-prettyAST cfg src = do
+prettyAST :: OrmoluConfig -> Text -> Text
+prettyAST cfg src = unsafePerformIO do
   let pfixityMap = O.packageFixityMap O.defaultDependencies
   (_, eSnippets) <-
     O.parseModule cfgWithDeltas pfixityMap "<input>" src
--- ormolu-live/build.sh
+++ ormolu-live/build.sh
@@ -6,6 +6,10 @@ trap 'rm -rf -- "$WDIR"' EXIT
 wasm32-wasi-cabal build exe:ormolu-live
 wasm32-wasi-cabal list-bin exe:ormolu-live
 ORMOLU_WASM="$(wasm32-wasi-cabal list-bin exe:ormolu-live)"
+
+"$(wasm32-wasi-ghc --print-libdir)"/post-link.mjs \
+  --input "$ORMOLU_WASM" --output "www/ghc_wasm_jsffi.js"
+
 wizer \
     --allow-wasi --wasm-bulk-memory true \
     "$ORMOLU_WASM" -o "$WDIR/ormolu-init.wasm" \
@@ -15,5 +19,12 @@ if [ $# -eq 0 ]; then
 else
     ORMOLU_WASM_OPT="$WDIR/ormolu-opt.wasm"
     wasm-opt "$@" "$WDIR/ormolu-init.wasm" -o "$ORMOLU_WASM_OPT"
+    wasm-strip "$ORMOLU_WASM_OPT"
 fi
-cp "$ORMOLU_WASM_OPT" src/ormolu.wasm
+
+rm -rf dist
+mkdir -p dist
+cp "$ORMOLU_WASM_OPT" dist/ormolu-live.wasm
+esbuild www/index.js --bundle --minify --platform=browser --format=esm --outfile=dist/index.js
+cp www/index.html dist/
+cp node_modules/bulma/css/bulma.min.css dist/
