504cce242fb1caacbc0339a9404e0032d5a1ad0f
Alexander Esgen <alexander.esgen@tweag.io>
2024-07-09 00:05:02 +0200

    Format `.cabal` files with cabal-gild

--- nix/format.sh
+++ nix/format.sh
@@ -2,10 +2,6 @@
 
 export LANG="C.UTF-8"
 
-cabal format
-(cd extract-hackage-info && cabal format)
-(cd ormolu-live && cabal format)
-
 export dirs="src app tests extract-hackage-info/src ormolu-live/app"
 # shellcheck disable=SC2046,SC2086
 ormolu -m inplace $(find $dirs -type f -name "*.hs" -o -name "*.hs-boot")
