e9ea55f196c91079cd3e751188af95e113e315d3
Alexander Esgen <alexander.esgen@tweag.io>
2024-10-16 21:00:31 +0200

    ghc-lib-parser 9.12

---
+++ data/examples/declaration/data/wildcard-binders.hs
@@ -0,0 +1,1 @@
+data Proxy _ = Proxy
--- data/examples/declaration/default/default.hs
+++ data/examples/declaration/default/default.hs
@@ -1,6 +1,13 @@
+module MyModule (default Monoid) where
+
 default        (  Int , Foo     , Bar      )
 
 default ( Int
                , Foo,
   Bar
            )
+
+default Num (Int, Float)
+default IsList ([], Vector)
+
+default IsString (Text.Text, Foundation.String, String)
