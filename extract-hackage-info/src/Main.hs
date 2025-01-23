tweag/ormolu

commit f5e1c67ae83f145b589ccf360e5d8ce16266e8f4

Update haskell.nix, update GHCs, update weeder
and use GHC 9.10 by default in preparation for ghc-lib-parser 9.12

extract-hackage-info/src/Main.hs
@@ -17,7 +17,6 @@ import Data.Binary.Get qualified as Binary
 import Data.Binary.Put qualified as Binary
 import Data.ByteString qualified as ByteString
 import Data.ByteString.Lazy qualified as BL
-import Data.List
 import Data.Map.Strict (Map)
 import Data.Map.Strict qualified as Map
 import Data.Maybe

.github/workflows/ci.yml
@@ -12,7 +12,7 @@ jobs:
     strategy:
       fail-fast: false
       matrix:
-        ghc: [ghc965, ghc982, ghc9101]
+        ghc: [ghc966, ghc984, ghc9101]
     name: Build and test on ${{ matrix.ghc }}
     runs-on: ubuntu-latest
     steps:

cabal.project
@@ -1,5 +1,6 @@
 packages: . extract-hackage-info
 
 tests: True
+multi-repl: True
 
 constraints: ormolu +dev

flake.nix
@@ -8,6 +8,7 @@
     nixpkgs.follows = "haskellNix/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     pre-commit-hooks.url = "github:cachix/git-hooks.nix";
+    weeder = { url = "github:ocharles/weeder"; flake = false; };
 
     # for Ormolu Live
     ghc-wasm-meta.url = "gitlab:ghc/ghc-wasm-meta?host=gitlab.haskell.org";
@@ -23,7 +24,7 @@
         inherit (pkgs) lib haskell-nix;
         inherit (haskell-nix) haskellLib;
 
-        ghcVersions = [ "ghc965" "ghc982" "ghc9101" ];
+        ghcVersions = [ "ghc9101" "ghc984" "ghc966" ];
         defaultGHCVersion = builtins.head ghcVersions;
         perGHC = lib.genAttrs ghcVersions (ghcVersion:
           let
@@ -41,10 +42,7 @@
             hackageTests = import ./expected-failures { inherit pkgs ormolu; };
             regionTests = import ./region-tests { inherit pkgs ormolu; };
             fixityTests = import ./fixity-tests { inherit pkgs ormolu; };
-            weeder = hsPkgs.tool "weeder" {
-              version = "2.6.0";
-              modules = [{ reinstallableLibGhc = false; }];
-            };
+            weeder = hsPkgs.tool "weeder" { src = inputs.weeder; };
             packages = lib.recurseIntoAttrs ({
               inherit ormolu;
               ormoluTests = haskellLib.collectChecks' hsPkgs;
@@ -143,7 +141,7 @@
             tools = {
               cabal = "latest";
               haskell-language-server = {
-                src = inputs.haskellNix.inputs."hls-2.8";
+                src = inputs.haskellNix.inputs."hls-2.9";
                 configureArgs = "--disable-benchmarks --disable-tests";
               };
             };

ormolu.cabal
@@ -5,8 +5,8 @@ license: BSD-3-Clause
 license-file: LICENSE.md
 maintainer: Mark Karpov <mark.karpov@tweag.io>
 tested-with:
-  ghc ==9.6.5
-  ghc ==9.8.2
+  ghc ==9.6.6
+  ghc ==9.8.4
   ghc ==9.10.1
 
 homepage: https://github.com/tweag/ormolu

commit 71577e2987a3ef33897679995b03ebdffd608cce

Restore the necessary Data.List import
It's needed for foldl' -- narrowed the import instead

Fixes: f5e1c67

extract-hackage-info/src/Main.hs
@@ -10,6 +10,7 @@
 module Main (main) where
 
 import Control.Exception
+import Control.Exception
 import Control.Monad
 import Data.Aeson qualified as A
 import Data.Binary qualified as Binary
@@ -17,6 +18,7 @@ import Data.Binary.Get qualified as Binary
 import Data.Binary.Put qualified as Binary
 import Data.ByteString qualified as ByteString
 import Data.ByteString.Lazy qualified as BL
+import Data.List (foldl')
 import Data.Map.Strict (Map)
 import Data.Map.Strict qualified as Map
 import Data.Maybe