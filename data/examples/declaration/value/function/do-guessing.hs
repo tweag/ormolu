648534fc4ad2c4da04608182449255b01bba3c35
thelissimus <thelissimus@tuta.io>
2026-03-21 13:52:48 +0500

    Fix rendering of consecutive blank lines in multiline strings

---
+++ data/examples/declaration/value/function/do-guessing.hs
@@ -0,0 +1,2 @@
+doGuessing :: (Ord t, Read t) => t -> IO ()
+doGuessing num = do {putStrLn "Enter your guess:"; guess <- getLine; case compare (read guess) num of {LT -> do {putStrLn "Too low!"; doGuessing num}; GT -> do { putStrLn "Too high!"; doGuessing num}; EQ -> putStrLn "You win!"}}
