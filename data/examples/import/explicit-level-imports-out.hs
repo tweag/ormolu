f19c8627ae42d0f45803c1deb51ce48cfda98b66
João Santos Reis <joao.reis@tweag.io>
2026-05-06 18:33:14 +0100

    Add examples for explicit level imports

---
+++ data/examples/import/explicit-level-imports-out.hs
@@ -0,0 +1,5 @@
+{-# LANGUAGE ExplicitLevelImports #-}
+
+import Data.ByteString (e)
+import Data.ByteString.Lazy quote (d)
+import splice Data.Text (a, b, c)
---
+++ data/examples/import/explicit-level-imports.hs
@@ -0,0 +1,5 @@
+{-# LANGUAGE ExplicitLevelImports #-}
+
+import splice Data.Text (a, b, c)
+import Data.ByteString.Lazy quote (d)
+import Data.ByteString (e)
