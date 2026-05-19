e4ce50bc67e02144e9be74ee743be7aa4da348c8
João Santos Reis <joao.reis@tweag.io>
2026-05-19 18:23:39 +0100

    Update explicit-level imports examples with additional import keywords

--- data/examples/import/explicit-level-imports-out.hs
+++ data/examples/import/explicit-level-imports-out.hs
@@ -1,5 +1,8 @@
 {-# LANGUAGE ExplicitLevelImports #-}
 
+import {-# SOURCE #-} safe qualified A splice as QA hiding (a, b, c, d, e, f)
+import quote qualified B as QB
+import qualified C splice as SC
 import Data.ByteString (e)
 import Data.ByteString.Lazy quote (d)
 import splice Data.Text (a, b, c)
--- data/examples/import/explicit-level-imports.hs
+++ data/examples/import/explicit-level-imports.hs
@@ -3,3 +3,6 @@
 import splice Data.Text (a, b, c)
 import Data.ByteString.Lazy quote (d)
 import Data.ByteString (e)
+import {-# SOURCE #-} safe qualified A splice as QA hiding (a, b, c, d, e, f)
+import quote qualified B as QB
+import qualified C splice as SC
