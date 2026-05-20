187518c08ee51e0145ae3f2d721987bc88ab64b3
Mark Karpov <mark.karpov@tweag.io>
2026-05-20 20:50:52 +0200

    Add a couple more test cases for ExplicitLevelImports

--- data/examples/import/explicit-level-imports.hs
+++ data/examples/import/explicit-level-imports.hs
@@ -6,3 +6,5 @@ import Data.ByteString (e)
 import {-# SOURCE #-} safe qualified A splice as QA hiding (a, b, c, d, e, f)
 import quote qualified B as QB
 import qualified C splice as SC
+import A splice
+import qualified D splice
