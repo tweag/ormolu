6743d25a6dacd5531ec6212cf195bc60f1c36304
João Santos Reis <joao.reis@tweag.io>
2026-07-06 11:14:47 +0100

    Add test case

---
+++ data/examples/declaration/value/function/case-with-comment-before-where.hs
@@ -0,0 +1,14 @@
+foo =
+  case x of
+    _ -> 1
+    -- comment
+    where
+      -- comment 2
+      x = 1
+
+foo = case x of
+    _ -> 1
+    -- comment
+    where
+      -- comment 2
+      x = 1
